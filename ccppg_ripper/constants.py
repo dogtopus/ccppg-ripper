0c5ba7074778a926cd8eea0ca4adb84890342af5
dogtopus <dogtopus@users.noreply.github.com>
2023-09-21 02:19:57 -0300

    Update pikepdf and use PyPI release of obsolete-cryptography

--- ccppg_ripper/constants.py
+++ ccppg_ripper/constants.py
@@ -6,11 +6,11 @@ URL_CATALOG_INDEX = 'http://202.96.31.36:8888/reading/list/{year}_pm_0_a5b1b3db-
 URL_CATALOG_INDEX_NOPAGE = 'http://202.96.31.36:8888/reading/list/{year}_pm_0_a5b1b3db-8656-4474-8055-fb3eb52bf458'
 URL_BOOK_CONTAINER = 'http://202.96.31.36:8888/Reading/Show/{book_uuid}'
 
-PREFIX_FLASH = 'flipbooks'
-PREFIX_HTML5 = 'fliphtml5'
-
 URL_BOOK_PREFIX = 'http://202.96.31.36:8888/{prefix}/password/main/qikan/etwx/{year}/{month}/{series}/web/'
 URL_BOOK_META = 'http://202.96.31.36:8888/{prefix}/password/main/qikan/etwx/{year}/{month}/{series}/web/{name}.xml'
+
+PREFIX_FLASH = 'flipbooks'
+PREFIX_HTML5 = 'fliphtml5'
 
 RE_BOOK_UUID_FROM_HOME = re.compile(r'^/Reading/Detail/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', flags=re.IGNORECASE)
 RE_BOOK_META = re.compile(r'^/(fliphtml5|flipbooks)/password/main/qikan/etwx/([^/]+)/([^/]+)/([^/]+)/web/([^/_]+)_opf_files')
